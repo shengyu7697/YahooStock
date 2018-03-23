7e15f4f18b48755f167ac673b35c7900820f18e6
shengyu <shengyu7697@gmail.com>
2018-03-23 11:06:15 +0800

    add YahooTWStock class

--- yahooStock.py
+++ yahooStock.py
@@ -4,50 +4,104 @@ import httplib
 import urllib2
 import re
 
+class YahooTWStock(object):
+    def __init__(self, stock_id):
+        self._id = stock_id
+        self._price = 0.0
+        self._name = ''
+        self.refresh()
+
+    def refresh(self):
+        self._request()
+
+    def get_id(self):
+        return self._id
+
+    def get_price(self):
+        return self._price
+
+    def get_name(self):
+        return self._name
+
+    def _request(self):
+        httplib.HTTPConnection.debuglevel = 1
+
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+        # For finding stock price
+        iRE_price = re.compile(r".*nowrap><b>([\d.]+)<.*", re.I | re.U | re.M | re.S)
+
+        # For finding stock name
+        # pattern: >2330台積電</a><br><a href="/pf/pfsel?stocklist=
+        e = ".*>\d+" + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
+        debug_print( e, "for name" )
+        iRE_name = re.compile(e, re.I | re.U | re.M | re.S)
+
+        # Get web page content
+        content = opener.open('http://tw.stock.yahoo.com/q/q?s=' + self._id).read()
+
+        # Print the whole content for debugging
+        #print content
+
+        match_price = iRE_price.match(content)
+        if str(match_price) == 'None':
+            print 'Not found'
+
+        match_name = iRE_name.match(content)
+        if str(match_name) == 'None':
+            print 'Not found'
+
+        self._price = float(match_price.groups()[0])
+        self._name = unicode(match_name.groups()[0], "BIG5")
+
 def debug_print( s, msg = None ):
     #print "[DEBUG]", msg, s
     pass
 
-httplib.HTTPConnection.debuglevel = 1
+def getStockInfo(stock_ids):
+    httplib.HTTPConnection.debuglevel = 1
 
-stock_ids = ( '2330', '2891', '2317', '0050' )
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    # For finding stock price
+    iRE_price = re.compile(r".*nowrap><b>([\d.]+)<.*", re.I | re.U | re.M | re.S)
 
-# For finding stock price
-iRE_price = re.compile( r".*nowrap><b>([\d.]+)<.*", \
-                        re.I | re.U | re.M | re.S )
+    # For finding stock name
+    # pattern: >2330台積電</a><br><a href="/pf/pfsel?stocklist=
+    e = ".*>\d+" + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
+    debug_print( e, "for name" )
+    iRE_name = re.compile(e, re.I | re.U | re.M | re.S)
 
-# For finding stock name
-# pattern: >2330台積電</a><br><a href="/pf/pfsel?stocklist=
-e = ".*>\d+" + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
-debug_print( e, "for name" )
-iRE_name = re.compile( e, re.I | re.U | re.M | re.S )
+    for stock_id in stock_ids:
+        # Get web page content
+        content = opener.open('http://tw.stock.yahoo.com/q/q?s=' + stock_id).read()
 
-for stock_id in stock_ids:
-    # Get web page content
-    content = opener.open( 'http://tw.stock.yahoo.com/q/q?s=' + \
-            stock_id ).read()
+        # Print the whole content for debugging
+        #print content
 
-    # Print the whole content for debugging
-    #print content
+        match_price = iRE_price.match(content)
+        if str(match_price) == 'None':
+            print 'Not found, continue ... '
+            continue
 
-    match_price = iRE_price.match( content )
-    if str(match_price) == 'None':
-        print 'Not found, continue ... '
-        continue
+        match_name = iRE_name.match(content)
+        if str(match_name) == 'None':
+            print 'Not found, continue ... '
+            continue
 
-    match_name = iRE_name.match( content )
-    if str(match_name) == 'None':
-        print 'Not found, continue ... '
-        continue
+        stock_price = match_price.groups()[0]
+        stock_name = unicode(match_name.groups()[0], "BIG5")
 
-    stock_price = match_price.groups()[ 0 ]
-    stock_name = unicode( match_name.groups()[ 0 ], "BIG5" )
+        # Print result
+        print "%s\t%s\t%.2f" % (stock_id, stock_name, float(stock_price))
 
-    # Print result
-    print "%s\t%s\t%.2f" % ( stock_id, stock_name, \
-            float( stock_price ) )
+if __name__ == '__main__':
+    yahoo = YahooTWStock('2330')
+    print "%s\t%s\t%.2f" % (yahoo.get_id(), yahoo.get_name(), yahoo.get_price())
+    
+    #stock_ids = ( '2330', '2891', '2317', '0050' )
+    #getStockInfo(stock_ids)
 
-#raw_input( "Press any key..." )
+    #raw_input( "Press any key..." )
