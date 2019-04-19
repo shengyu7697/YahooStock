9d02f2662cdd9ded858ed25b312c633ae1dcc7a7
shengyu <shengyu7697@gmail.com>
2019-04-19 09:16:33 +0800

    Tab => 4 spaces

--- getStockFromYahoo.py
+++ getStockFromYahoo.py
@@ -6,43 +6,43 @@ import re
 import chardet
 
 def getStockFromYahoo(id):
-	r = requests.get('http://tw.stock.yahoo.com/q/q?s=%s' % id)
-	content = r.content
-	
-	# 根據傳進來的參數自動辨別編碼格式，然後進行相應的解碼
-	encode_type = chardet.detect(content)
-	content = content.decode(encode_type['encoding'])
+    r = requests.get('http://tw.stock.yahoo.com/q/q?s=%s' % id)
+    content = r.content
 
-	# For finding stock price
-	iRE_price = re.compile(r'.*nowrap><b>([\d.]+)<.*', re.I | re.U | re.M | re.S)
+    # 根據傳進來的參數自動辨別編碼格式，然後進行相應的解碼
+    encode_type = chardet.detect(content)
+    content = content.decode(encode_type['encoding'])
 
-	# For finding stock name
-	# pattern: >2330台積電</a><br><a href="/pf/pfsel?stocklist=
-	e = '.*>\d+' + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
-	#debug_print(e)
-	iRE_name = re.compile(e, re.I | re.U | re.M | re.S)
+    # For finding stock price
+    iRE_price = re.compile(r'.*nowrap><b>([\d.]+)<.*', re.I | re.U | re.M | re.S)
 
-	# Print the whole content for debugging
-	#print(content)
+    # For finding stock name
+    # pattern: >2330台積電</a><br><a href="/pf/pfsel?stocklist=
+    e = '.*>\d+' + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
+    #debug_print(e)
+    iRE_name = re.compile(e, re.I | re.U | re.M | re.S)
 
-	match_price = iRE_price.match(content)
-	if str(match_price) == 'None':
-		print('Not found')
+    # Print the whole content for debugging
+    #print(content)
 
-	match_name = iRE_name.match(content)
-	if str(match_name) == 'None':
-		print('Not found')
+    match_price = iRE_price.match(content)
+    if str(match_price) == 'None':
+        print('Not found')
 
-	_price = float(match_price.groups()[0])
-	_name = match_name.groups()[0]
-	return _price, _name
+    match_name = iRE_name.match(content)
+    if str(match_name) == 'None':
+        print('Not found')
+
+    _price = float(match_price.groups()[0])
+    _name = match_name.groups()[0]
+    return _price, _name
 
 def debug_print(msg):
-	tag = 'DEBUG'
-	print('[%s] %s' % (tag, msg))
+    tag = 'DEBUG'
+    print('[%s] %s' % (tag, msg))
 
 if __name__ == '__main__':
-	price, name = getStockFromYahoo(2330)
+    price, name = getStockFromYahoo(2330)
 
-	print(price)
-	print(name)
+    print(price)
+    print(name)
--- getYahooStockByBeautifulSoup.py
+++ getYahooStockByBeautifulSoup.py
@@ -7,25 +7,25 @@ import webbrowser
 import sys
 
 def parseHtml(content):
-	title = content.get('title')
-	url = content.get('href')
-	#style = content.get('style')
-	#style = content.find('url').getText()
-	#print title
-	#print url
-	return url
+    title = content.get('title')
+    url = content.get('href')
+    #style = content.get('style')
+    #style = content.find('url').getText()
+    #print title
+    #print url
+    return url
 
 def parseYahooPage(id1):
-	r = requests.get('http://tw.stock.yahoo.com/q/q?s=%s' % id1)
-	c = r.content
+    r = requests.get('http://tw.stock.yahoo.com/q/q?s=%s' % id1)
+    c = r.content
 
-	#soup = BeautifulSoup(c, "html.parser")
-	soup = BeautifulSoup(c, 'lxml')
-	#print(soup.prettify())
+    #soup = BeautifulSoup(c, "html.parser")
+    soup = BeautifulSoup(c, 'lxml')
+    #print(soup.prettify())
 
-	table = soup.find_all('table')[1].find_all('b')
-	print(table)
-	#print "found %d links" % len(links)
+    table = soup.find_all('table')[1].find_all('b')
+    print(table)
+    #print "found %d links" % len(links)
 
 if __name__ == '__main__':
-	parseYahooPage(2330)
+    parseYahooPage(2330)
